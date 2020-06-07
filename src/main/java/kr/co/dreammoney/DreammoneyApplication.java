76a4f7f0e44e7fb8eae9b48ae267899f9c3e287e
yoon pyo, hong <ghddbsvy@gmail.com>
2020-06-07 17:10:01 +0900

    init project.

---
+++ src/main/java/kr/co/dreammoney/DreammoneyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dreammoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreammoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreammoneyApplication.class, args);
+    }
+
+}
